3bed50a5b7476882f68d6283a406e44d5a31aa74
Elo2006 <73569242+Elo2006@users.noreply.github.com>
2021-01-22 17:43:35 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var database, form, player, game, playerCount;
+var gameState = 0
+var allPlayers
+var car1, car2, car3, car4, cars
+
+function setup(){
+    createCanvas(displayWidth, displayHeight);
+    database = firebase.database()
+    game = new Game()
+    game.getState()
+    game.start()
+
+}
+
+
+function draw(){
+   if(playerCount === 4){
+game.update(1)
+   }
+   if(gameState === 1){
+clear()
+game.play()
+
+   }
+}
+
+
